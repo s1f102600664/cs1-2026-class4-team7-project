6b04efb6af609928a582525185d83176fac35c3d
s1f102600664 <s1f102600664@iniad.org>
2026-06-25 19:24:29 +0900

    ページネーションを修正
    何回押しても正常動作するようにjavascriptを修正しました。

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,11 +1,10 @@
 function pagination1() {
-    pagination1.off()
-    pagination2.off()
-    article_link1.innerHTML = '<a id="article_link1" href="article1.html">記事1</a></p>';
+    let element = document.getElementById('article_link1');
+    element.innerHTML = '<a id="article_link1" href="article1.html">記事1</a></p>';
 }
 
 function pagination2() {
-    pagination1.off()
-    pagination2.off()
-    article_link1.innerHTML = '<a id="article_link1" href="article8.html">記事8</a></p>';
+    let element = document.getElementById('article_link1');
+    element.innerHTML = '<a id="article_link1" href="article8.html">記事8</a></p>';
+
 }
