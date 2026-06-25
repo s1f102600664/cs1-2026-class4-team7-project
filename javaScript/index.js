9cf588967795c94f54f63cb7dabd677dc07c44f1
s1f102600664 <s1f102600664@iniad.org>
2026-06-25 12:42:26 +0900

    実際にページネーションを作りました。
    ボタンを押すと記事1から記事8に変換するjavascriptを作りました。

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,4 @@
 function pagination() {
-    let e = document.querySelector('.row_article');
-    e.innerHTML = '<h3>変更しました。</h3>';
+    // let e = document.querySelector('.row_article');
+    article_link1.innerHTML = '<a id="article_link1" href="article1.html">記事8</a></p>';
 }
