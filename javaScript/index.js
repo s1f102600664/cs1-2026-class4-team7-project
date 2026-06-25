de7aa2353e071937e0ea04b986cfe61c3c44f005
s1f102600664 <s1f102600664@iniad.org>
2026-06-25 14:45:42 +0900

    試しにボタンの関数停止プログラムを追加

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,4 +1,11 @@
-function pagination() {
-    // let e = document.querySelector('.row_article');
-    article_link1.innerHTML = '<a id="article_link1" href="article1.html">記事8</a></p>';
+function pagination1() {
+    pagination1.off()
+    pagination2.off()
+    article_link1.innerHTML = '<a id="article_link1" href="article1.html">記事1</a></p>';
+}
+
+function pagination2() {
+    pagination1.off()
+    pagination2.off()
+    article_link1.innerHTML = '<a id="article_link1" href="article8.html">記事8</a></p>';
 }
