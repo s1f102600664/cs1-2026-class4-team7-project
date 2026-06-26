2627c741a006159b68f51fbe4a6612804039172e
s1f102600664 <s1f102600664@iniad.org>
2026-06-27 00:42:13 +0900

    ボタンを押すと記事全体が他の記事に変わるようにしました。
    <p>, href, a内の文字が変わるようにしました。

--- javaScript/index.js
+++ javaScript/index.js
@@ -4,4 +4,7 @@
 
 function pagination2() {
     document.getElementById("article_picture1").src = "images/20260606_tamanegi_5.jpg";
+    document.getElementById("article_link1").innerHTML = "記事8";
+    document.getElementById("article_link1").href = "article8.html"
+    document.getElementById("brief1").innerHTML = "8"
 }
