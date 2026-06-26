80d66882ac6c066ee899ec8c5a7514259fbcb5a7
s1f102600664 <s1f102600664@iniad.org>
2026-06-27 00:14:32 +0900

    写真がボタンを押すと変わる仕組みが正常に動作するようにしました。
    javascriptとhtmlをリンク
    buttonの<p>囲みを解除
    javascriptのコードを修正

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,12 +1,7 @@
-function pagination1() {
-    let element = document.getElementById('article_link1');
-    element.innerHTML = '<a id="article_link1" href="article1.html">記事1</a></p>';
-}
+// function pagination1() {
+   
+// }
 
 function pagination2() {
-    let element = document.getElementById('article_link1');
-    element.innerHTML = '<a id="article_link1" href="article8.html">記事8</a></p>';
-    // document.getElementById('article_1').src = 'images/20260528_tamanegi_1.jpg';
-
-
+    document.getElementById("article_picture1").src = "images/20260606_tamanegi_5.jpg";
 }
