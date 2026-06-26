2318d8311ef5a75afaede45f8ca3926b886eaba3
s1f102600664 <s1f102600664@iniad.org>
2026-06-26 14:59:37 +0900

    #idを追加

--- javaScript/index.js
+++ javaScript/index.js
@@ -6,5 +6,7 @@ function pagination1() {
 function pagination2() {
     let element = document.getElementById('article_link1');
     element.innerHTML = '<a id="article_link1" href="article8.html">記事8</a></p>';
+    document.getElementById('article_1').src = 'images/20260528_tamanegi_1.jpg';
+
 
 }
