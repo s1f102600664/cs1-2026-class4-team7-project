38d88d3e479da999e8b5efa0b71d33525b5ab2f6
s1f102600664 <s1f102600664@iniad.org>
2026-06-28 22:30:29 +0900

    試しページネーション時にもアニメーションが起きるようにコード追加(調整中)

--- javaScript/index.js
+++ javaScript/index.js
@@ -5,6 +5,13 @@
 function pagination2() {
     document.getElementById("article_picture1").src = "images/20260606_tamanegi_5.jpg";
     document.getElementById("article_link1").innerHTML = "記事8";
-    document.getElementById("article_link1").href = "article8.html"
-    document.getElementById("brief1").innerHTML = "8"
+    document.getElementById("article_link1").href = "article8.html";
+    document.getElementById("brief1").innerHTML = "8";
 }
+
+
+const e = document.getElementById("show_article");
+const s = document.getElementById("change_button2");
+s.addEventListener("click", () => {e.classList.add("_jump");}); 
+s.addEventListener("animationed", () => {e.classList.remove("_jump");}); 
+s.addEventListener("animationcancel", () => {e.classList.remove("_jump");}); 
